b4e323a4717404ab72ae6076ba8e2ce1bf3542b8
munirdevs08 <munirdevs08@gmail.com>
2025-08-26 08:00:34 +0700

    update homepage view

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\PemerintahanController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingController::class, 'index_landing']);
+Route::get('/home', [HomepageController::class, 'index']);
 Route::get('/potensi/pariwisata', [PostController::class, 'postsList']);
 Route::get('/pemerintahan/bupati', [PemerintahanController::class, 'bupati']);
 Route::get('/pemerintahan/lembaga-legislatif', [PemerintahanController::class, 'legislatif']);
+
+
